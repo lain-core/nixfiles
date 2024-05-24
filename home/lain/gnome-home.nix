835e5104464e60f5ef239af9c960b83e2ab91a55
lain <hking0036@gmail.com>
2024-05-23 20:52:41 -0400

    fleshed out gnome-home

--- home/lain/gnome-home.nix
+++ home/lain/gnome-home.nix
@@ -28,5 +28,43 @@
         "org.telegram.desktop.desktop"
       ];
     };
+
+    "org/gnome/mutter" = {
+      "edge-tiling" = true;
+    };
+
+    "org/gnome/desktop/interface" = {
+      "clock-show-weekday" = true;
+      "color-scheme" = "prefer-dark";
+      "show-battery-percentage" = true;
+    };
+
+    "org/gnome/desktop/peripherals/mouse" = {
+      "natural-scroll" = false;
+    };
+
+    "org/gnome/desktop/peripherals/touchpad" = {
+      "click-method" = "areas"; 
+      "natural-scroll" = false;
+      "two-finger-scrolling-enabled" = true;
+    };
+
+    "org/gtk/settings/file-chooser" = {
+      "sort-directories-first" = true;
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      "custom-keybindings" = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0 " ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings" = {
+      "custom-keybindings" = {
+        "custom0" = {
+          "binding" = "<Control><Alt>t";
+          "command" = "kgx";
+          "name" = "Terminal";
+        };
+      };
+    };
   };
 }
