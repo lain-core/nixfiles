ab5683ea04f02506b6e411ca75d4355cb6d5f5d7
lain <hking0036@gmail.com>
2024-05-23 20:56:27 -0400

    correct hotkey binding

--- home/lain/gnome-home.nix
+++ home/lain/gnome-home.nix
@@ -54,17 +54,13 @@
     };
 
     "org/gnome/settings-daemon/plugins/media-keys" = {
-      "custom-keybindings" = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0 " ];
+      "custom-keybindings" = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" ];
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings" = {
-      "custom-keybindings" = {
-        "custom0" = {
-          "binding" = "<Control><Alt>t";
-          "command" = "kgx";
-          "name" = "Terminal";
-        };
-      };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      "binding" = "<Control><Alt>t";
+      "command" = "kgx";
+      "name" = "Terminal";
     };
   };
 }
